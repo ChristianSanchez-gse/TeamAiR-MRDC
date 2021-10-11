53e936185ffb2f01af01bba3589b769e0bad901c
abeige2 <31280510+abeige@users.noreply.github.com>
2021-10-11 16:48:36 -0500

    more lines

--- Codebase/testing-adam/test1.py
+++ Codebase/testing-adam/test1.py
@@ -1,1 +1,16 @@
+def func1():
+    print("Function 1")
+
+def power(a, b=1):
+    result = 1
+    for i in range(b):
+        result *= a
+        return result
+
 print("Hello world!")
+x = 0
+print(x)
+x = "hello"
+print(x)
+func1()
+print(power(2,4))
