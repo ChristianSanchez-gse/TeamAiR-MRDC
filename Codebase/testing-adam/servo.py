47fe614829a8fedba82b8e58c9775276a5ccdb29
abeige2 <31280510+abeige@users.noreply.github.com>
2021-11-06 13:45:14 -0500

    servo testing

---
+++ Codebase/testing-adam/servo.py
@@ -0,0 +1,15 @@
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(18)
+
+try:
+    while True:
+        servo.min()
+        sleep(0.5)
+        servo.mid()
+        sleep(0.5)
+        servo.max()
+        sleep(0.5)
+except KeyboardInterrupt:
+        print("Program stopped")
