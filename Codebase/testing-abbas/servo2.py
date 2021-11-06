881eb3e80e08d90d97194d506768a61373ce01c8
amadni2 <amadni2@uic.edu>
2021-11-06 14:09:06 -0500

    edited servo2

--- Codebase/testing-abbas/servo2.py
+++ Codebase/testing-abbas/servo2.py
@@ -5,11 +5,11 @@ servo = Servo(18)
 
 try:
         while True:
-        servo.min()
-        sleep(0.5)
-        servo.mid()
-        sleep(0.5)
-        servo.max()
-        sleep(0.5)
+        	servo.min()
+        	sleep(0.5)
+        	servo.mid()
+        	sleep(0.5)
+        	servo.max()
+        	sleep(0.5)
 except KeyboardInterrupt:
         print("Program stopped")
