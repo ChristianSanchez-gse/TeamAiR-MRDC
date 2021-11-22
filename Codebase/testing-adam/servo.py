7616fe61f50940f237897ad2dbbb228c2bbea3e0
abeige2 <31280510+abeige@users.noreply.github.com>
2021-11-22 16:37:01 -0600

    functions for open and close

--- Codebase/testing-adam/servo.py
+++ Codebase/testing-adam/servo.py
@@ -4,16 +4,19 @@ from time import sleep
 servo1 = Servo(18)
 servo2 = Servo(13)
 
+def open:
+    servo1.min()
+    servo2.max()
+
+def close:
+    servo1.max()
+    servo2.min();
+
 try:
     while True:
-        servo1.min()
-        servo2.max()
+        open()
         sleep(1.0)
-        servo1.mid()
-        servo2.mid();
-        sleep(1.0)
-        servo1.max()
-        servo2.min();
+        close()
         sleep(1.0)
 except KeyboardInterrupt:
         print("Program stopped")
