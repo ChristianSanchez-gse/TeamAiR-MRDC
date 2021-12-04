7284718ba441185dd1c54c84521001fcdfeb727e
abeige <31280510+abeige@users.noreply.github.com>
2021-12-04 14:30:46 -0600

    more info

--- Codebase/testing-adam/color_sensor.py
+++ Codebase/testing-adam/color_sensor.py
@@ -8,15 +8,15 @@ sensor = adafruit_tcs34725.TCS34725(i2c)
 # loop through to keep getting values
 while True:
     # Raw data from the sensor in a 4-tuple of red, green, blue, clear light component values
-    print(sensor.color_raw)
+    # print(sensor.color_raw)
 
     color = sensor.color
     color_rgb = sensor.color_rgb_bytes
     print("RGB color as 8 bits per channel int: #{0:02X} or as 3-tuple: {1}".format(color, color_rgb))
-    # print("RGB to human language value: ", webcolors.rgb_to_name(color_rgb))
+    print("  ", webcolors.rgb_to_name(color_rgb))
     # Read the color temperature and lux of the sensor too.
-    # temp = sensor.color_temperature
-    # lux = sensor.lux
-    # print("Temperature: {0}K Lux: {1}\n".format(temp, lux))
+    temp = sensor.color_temperature
+    lux = sensor.lux
+    print("  Temperature: {0}K Lux: {1}\n".format(temp, lux))
     # Delay for a second and repeat.
     time.sleep(1.0)
