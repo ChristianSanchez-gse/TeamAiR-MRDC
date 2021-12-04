279935474be6bbb761bb91d01e6a7fad55af8e88
abeige <31280510+abeige@users.noreply.github.com>
2021-12-04 15:44:10 -0600

    getHue function

--- Codebase/testing-adam/color_sensor.py
+++ Codebase/testing-adam/color_sensor.py
@@ -5,13 +5,7 @@ import adafruit_tcs34725
 i2c = board.I2C()
 sensor = adafruit_tcs34725.TCS34725(i2c)
 
-# loop through to keep getting values
-while True:
-    # Raw data from the sensor in a 4-tuple of red, green, blue, clear light component values
-    # print(sensor.color_raw)
-    input("Press Enter to read color...")
-    color = sensor.color
-    color_rgb = sensor.color_rgb_bytes
+def getHue(color_rgb):
     R = color_rgb[0] / 255
     G = color_rgb[1] / 255
     B = color_rgb[2] / 255
@@ -21,25 +15,36 @@ while True:
     max = -1
     max_index = -1
     min = 256
-    min_index = -1
     for i in range(3):
         if (new_rgb[i] > max):
             max = new_rgb[i]
             max_index = i
         if (new_rgb[i] < min):
             min = new_rgb[i]
-            min_index = i
 
     hue = -1
     if (max_index == 0):
         hue = (G-B)/(max-min)
-    if (max_index == 1):
+    elif (max_index == 1):
         hue = 2.0 + (B-R)/(max-min)
-    if (max_index == 2):
+    elif (max_index == 2):
         hue = 4.0 + (R-G)/(max-min)
 
-    hue = hue * 60
-    print("RGB: ", color_rgb)
+    hue *= 60
+    if hue < 0:
+        hue += 360
+    return hue
+
+
+# loop through to keep getting values
+while True:
+    # Raw data from the sensor in a 4-tuple of red, green, blue, clear light component values
+    # print(sensor.color_raw)
+    input("Press Enter to read color...")
+    color = sensor.color
+    color_rgb = sensor.color_rgb_bytes
+
+    hue = getHue(color_rgb)
     print("R: {0}, G: {1}, B; {2}".format(R, G, B))
     print("Hue: ", hue)
 
