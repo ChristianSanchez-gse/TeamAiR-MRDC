6820969ace51beb436a82c82c5c85bfdc4f91c7a
abeige <31280510+abeige@users.noreply.github.com>
2021-12-04 18:16:11 -0600

    file reading

--- Codebase/testing-adam/color_sensor.py
+++ Codebase/testing-adam/color_sensor.py
@@ -7,13 +7,23 @@ sensor = adafruit_tcs34725.TCS34725(i2c)
 
 from colors import *
 
+filename = input("Enter a filename: ")
+
+f = open(filename, "a")
+
 # loop through to keep getting values
-while True:
+continue = True
+while continue:
     # Raw data from the sensor in a 4-tuple of red, green, blue, clear light component values
     # print(sensor.color_raw)
-    input("Press enter to read color...")
+    user_input = input("Press enter to read color or # to stop: ")
+    if user_input == "#"
+        continue = False
     color_rgb = sensor.color_rgb_bytes
     temp = round(sensor.color_temperature)
     hue = round(getHue(color_rgb))
 
     print("Hue: {0}, Temperature: {1}".format(hue, temp))
+    f.write("{0} {1}".format(hue, temp))
+
+f.close();
