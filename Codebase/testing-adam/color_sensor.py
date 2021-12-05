8270f42cca43a4459f42ab12f72ca7ead79de8e3
abeige <31280510+abeige@users.noreply.github.com>
2021-12-04 18:28:16 -0600

    clean and comment

--- Codebase/testing-adam/color_sensor.py
+++ Codebase/testing-adam/color_sensor.py
@@ -1,31 +1,31 @@
-import time # to check color every certain period of time
-import webcolors # for human color names
+import time  # to check color every certain period of time
 import board
 import adafruit_tcs34725
 i2c = board.I2C()
 sensor = adafruit_tcs34725.TCS34725(i2c)
 
-from colors import *
+from colors import *  # functions I wrote
 
+# open file to write to
 filename = input("Enter a filename: ")
-
 f = open(filename, "a")
 
-# loop through to keep getting values
 run = True
 while run:
-    # Raw data from the sensor in a 4-tuple of red, green, blue, clear light component values
-    # print(sensor.color_raw)
+    # prompt user to read color or stop program
     user_input = input("Press enter to read color or # to stop: ")
     if (user_input == "#"):
         run = False
         break
 
+    # read what's in front of the sensor and calculate the hue and temperature
     color_rgb = sensor.color_rgb_bytes
     temp = round(sensor.color_temperature)
     hue = round(getHue(color_rgb))
 
+    # print out info and write hue and temp to file
     print("Hue: {0}, Temperature: {1}".format(hue, temp))
     f.write("{0} {1}\n".format(hue, temp))
 
+# close the file
 f.close();
