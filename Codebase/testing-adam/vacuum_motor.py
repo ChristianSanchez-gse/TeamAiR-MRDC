fcb1becc0b4905edacc86c38e550edbef2b78961
abeige <abeige2@uic.edu>
2022-02-08 14:17:48 -0600

    stop motor

--- Codebase/testing-adam/vacuum_motor.py
+++ Codebase/testing-adam/vacuum_motor.py
@@ -10,17 +10,6 @@ time.sleep(5)
 for i in range(3):
         pi.set_servo_pulsewidth(motor_pin, 1500)
         time.sleep(1)
-        pi.set_servo_pulsewidth(motor_pin, 1600)
-        time.sleep(1)
-        pi.set_servo_pulsewidth(motor_pin, 1700)
-        time.sleep(1)
-        pi.set_servo_pulsewidth(motor_pin, 1800)
-        time.sleep(1)
-        pi.set_servo_pulsewidth(motor_pin, 1900)
-        time.sleep(1)
-        pi.set_servo_pulsewidth(motor_pin, 2000)
-        time.sleep(1)
-
 
 pi.set_servo_pulsewidth(motor_pin, 0)
 pi.stop()
