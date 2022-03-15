7c0a72377d9a72dcd8b87311dceaabc64c59e707
TeamAirPi <edt.teamair@gmail.com>
2022-03-15 16:25:50 -0500

    wrote get closest color

--- Codebase/production/colors.py
+++ Codebase/production/colors.py
@@ -10,43 +10,7 @@ green = (8, 41, 7)
 blue = (2, 18, 47)
 purple = (13, 11, 27)
 pink = (43, 6, 1)
-
-# convert rgb to hue
-def getHue(color_rgb):
-    R = color_rgb[0] / 255
-    G = color_rgb[1] / 255
-    B = color_rgb[2] / 255
-
-    new_rgb = (R, G, B)
-
-    max = -1
-    max_index = -1
-    min = 256
-    for i in range(3):
-        if (new_rgb[i] > max):
-            max = new_rgb[i]
-            max_index = i
-        if (new_rgb[i] < min):
-            min = new_rgb[i]
-
-    hue = -1
-    if (max_index == 0):
-        hue = (G-B)/(max-min)
-    elif (max_index == 1):
-        hue = 2.0 + (B-R)/(max-min)
-    elif (max_index == 2):
-        hue = 4.0 + (R-G)/(max-min)
-
-    hue *= 60
-    if hue < 0:
-        hue += 360
-    return hue
-
-# get the hue and temp from the color sensor
-def readColorSensor(sensor):
-    hue = round(getHue(sensor.color_rgb_bytes))
-    temp = round(sensor.color_temperature)
-    return hue, temp
+colorsRGB = (red, orange, yellow, green, blue, purple, pink)
 
 # read color 10 times, get the average, then return a string color depending on hue and temp values
 def getBallColor(sensor):
@@ -65,4 +29,7 @@ def getBallColor(sensor):
     return getClosestColor(rgbAverage)
 
 def getClosestColor(rgbAverage):
-    pass
+    distances = []
+    for c in colorsRGB:
+        distances.append(np.linalg.norm(c - rgbAverage))
+    return min(distances)
