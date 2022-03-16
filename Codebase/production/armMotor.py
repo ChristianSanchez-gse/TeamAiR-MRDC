ee1c8363aeac4c1b6f14cf7372c6b2133ddfa936
abeige <abeige2@uic.edu>
2022-03-16 11:34:23 -0500

    make time to plug in battery longer

--- Codebase/production/armMotor.py
+++ Codebase/production/armMotor.py
@@ -3,10 +3,10 @@ from time import sleep
 motor = Servo(18)
 
 motor.value = -1
-for i in range(5):
-    print(5-i)
+for i in range(8):
+    print(8-i)
     sleep(1)
 
-motor.value = -0.4
+motor.value = -0.6
 sleep(1)
 motor.value = -1
