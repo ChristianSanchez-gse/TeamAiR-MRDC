8644b5dce7639991bc53f1926e98dfb389d41dc7
abeige <abeige2@uic.edu>
2022-03-19 09:40:03 -0500

    use a dictionary for colors and update minColor as we go instead of creating an array

--- Codebase/production/colors.py
+++ Codebase/production/colors.py
@@ -10,27 +10,42 @@ green = (8, 41, 7)
 blue = (2, 18, 47)
 purple = (13, 11, 27)
 pink = (43, 6, 1)
-colorsRGB = (red, orange, yellow, green, blue, purple, pink)
-colorNames = ("red", "orange", "yellow", "green", "blue", "purple", "pink")
 
-# read color 10 times, get the average, then return a string color depending on hue and temp values
+colors = {"red":red,
+          "orange":orange,
+          "yellow":yellow,
+          "green":green,
+          "blue":blue,
+          "purple":purple,
+          "pink":pink}
+
+# read color 10 times, get the average, then return a string color closest to that average
 def getBallColor(sensor):
     rgbReadings = []
+    numReadings = 10
 
     print("getting average...")
 
-    for i in range(10):
+    for i in range(numReadings):
         rgbReadings.append(sensor.color_rgb_bytes)
         sleep(0.1)
-    
+
     print("done")
 
-    rgbAverage = np.mean(rgbReadings, axis=0)
+    rgbAverage = sum(rgbReadings) / numReadings
 
     return getClosestColor(rgbAverage)
 
 def getClosestColor(rgbAverage):
-    distances = []
-    for c in colorsRGB:
-        distances.append(np.linalg.norm(c - rgbAverage))
-    return colorNames[distances.index(min(distances))]
+    minDistance = 444  # farthest possible distance is sqrt(256^2 + 256^2 + 256^2) = 443.4
+    minColor = ""
+
+    # calculate euclidean distance between each color and rgbAverage,
+    # updating min as we go
+    for key, value in colors:
+        distance = np.linalg.norm(value - rgbAverage)
+        if distance < minDistance:
+            minColor = key
+
+    # return string of closest color
+    return minColor
