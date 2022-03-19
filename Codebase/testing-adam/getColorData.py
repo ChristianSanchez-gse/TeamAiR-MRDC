080da6aa17a106f2b54dcda2737b94164bc3bd8a
abeige <abeige2@uic.edu>
2022-03-19 09:53:10 -0500

    clean up files

--- Codebase/testing-adam/getColorData.py
+++ Codebase/testing-adam/getColorData.py
@@ -8,23 +8,18 @@ from colors import *  # functions I wrote
 
 # open file to write to
 filename = input("Enter a filename: ")
-f = open(filename, "a")
+f = open("color-data/" + filename, "w")
 
-run = True
-while run:
+while True:
     # prompt user to read color or stop program
     user_input = input("Press enter to read color or # to stop: ")
     if (user_input == "#"):
-        run = False
         break
 
-    # read what's in front of the sensor and calculate the hue and temperature
+    # read what's in front of the sensor
     color_rgb = sensor.color_rgb_bytes
-    temp = round(sensor.color_temperature)
-    hue = round(getHue(color_rgb))
 
-    # print out info and write hue and temp to file
-    print("Hue: {0}, Temperature: {1}".format(hue, temp))
+    # print rgb values and write to file
     print("RGB: {0}".format(color_rgb))
     f.write("{0} {1} {2}\n".format(color_rgb[0], color_rgb[1], color_rgb[2]))
 
--- Codebase/testing-adam/get_hue_temp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cat $1 | awk '{print $1}'
-cat $1 | awk '{print $2}'
